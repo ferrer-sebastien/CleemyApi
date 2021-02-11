110616b316aae73404a835fd28c93b8cfb8aa86d
ferrer-sebastien <ferrer.sebastien94@gmail.com>
2021-02-11 01:15:49 +0100

    Init Db With EF and seed data
    Init Db With EF and seed data

---
+++ CleemyApi/Cleemy.Expenses.Data/Migrations/20210210235940_TestMigrationWithInitData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cleemy.Expenses.Data.Migrations
+{
+    public partial class TestMigrationWithInitData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+    }
+}
--- CleemyApi/Cleemy.Expenses.Data/Persistence/ExpensesContext.cs
+++ CleemyApi/Cleemy.Expenses.Data/Persistence/ExpensesContext.cs
@@ -15,5 +15,26 @@ namespace Cleemy.Expenses.Business
 
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User
+                    {
+                        Id = 2,
+                        FirstName = "Stark",
+                        SecondName = "Anthony",
+                        Currency = "USD"
+                    },
+                    new User
+                    {
+                        Id = 3,
+                        FirstName = "Romanova",
+                        SecondName = "Natasha",
+                        Currency = "RUB"
+                    }
+                );
+        }
     }
 }
